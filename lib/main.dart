4eb4a0b0b7e4a6930674d546a339dcff6086f64a
NdegwaJulius <juliusndegwa2@gmail.com>
2023-02-26 12:27:04 +0300

    Convert the http.Response to an Album

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,51 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-Future<http.Response> fetchAlbum() {
-  return http.get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+Future<Album> fetchAlbum() async {
+  final response = await http
+      .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return Album.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
+}
+
+List<Album> albumFromJson(String str) =>
+    List<Album>.from(json.decode(str).map((x) => Album.fromJson(x)));
+
+String albumToJson(List<Album> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class Album {
+  Album({
+    required this.userId,
+    required this.id,
+    required this.title,
+  });
+
+  int userId;
+  int id;
+  String title;
+
+  factory Album.fromJson(Map<String, dynamic> json) => Album(
+        userId: json["userId"],
+        id: json["id"],
+        title: json["title"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "userId": userId,
+        "id": id,
+        "title": title,
+      };
 }
 
 void main() {
